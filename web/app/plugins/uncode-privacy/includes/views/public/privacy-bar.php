09471745dc239b7fd0bcce97a24765ac50d4c2bc
Thomas Wickham <mackwic@gmail.com>
2020-04-08 18:34:33 +0200

    add theme plugins

---
+++ web/app/plugins/uncode-privacy/includes/views/public/privacy-bar.php
@@ -0,0 +1,13 @@
+<div class="gdpr gdpr-privacy-bar" style="display:none;">
+	<div class="gdpr-wrapper">
+		<div class="gdpr-content">
+			<p>
+				<?php echo nl2br( wp_kses_post( $content ) ); ?>
+			</p>
+		</div>
+		<div class="gdpr-right">
+			<button class="gdpr-preferences" type="button">Privacy settings</button>
+			<button class="gdpr-agreement btn-accent btn-flat" type="button">I Accept</button>
+		</div>
+	</div>
+</div>
